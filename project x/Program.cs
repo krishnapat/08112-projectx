0292320b84b8b7e7b58deccf78006d338d58cd00
krishnapat <krishna_patel_21@hotmail.com>
2015-05-06 13:41:55 +0100

    Project

---
+++ project x/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_x
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // program projectx
+            string x;
+            bool f;
+            int a,z,g;
+            Console.WriteLine("Enter input");
+            string input;
+            input = Console.ReadLine();
+            x = input;
+            g = input.Length;
+            z = g -1;
+            a = 0;
+            if (g == 1) 
+            {
+                f = true;
+            }     
+        f = true;
+
+        while (f = true & (a < z))
+        {
+            //while character at a of x is one of 
+            while (x[a] == ' ' || x[a] == ',' || x[a] == '!' || x[a] == '?' || x[a] == '.')
+            {
+                a++;
+            }
+            while (x[z] == ' ' || x[z] == ',' || x[z] == '!' || x[z] == '?' || x[z] == '.')
+            {
+                z--;
+            }
+            string wq = x.ToUpper();
+            if ( wq[a] == wq[z]  )
+            {
+                z--;
+                a++;
+            }
+            else
+            {
+                f = false;
+            }
+        }
+            //if the toUpperCase of character at a of x is identical to the
+            //toUpperCase of character at z of x then
+            //a++ and z--;
+            //else
+            //f=0;
+            //if f=0 then
+            //print out appropriate message
+            //else
+            //print out other appropriate message
+        }
+    }
+}
